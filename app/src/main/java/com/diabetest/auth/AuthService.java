cd9da8f76de3abc3fce43ebbdcfacb347e739e1a
ilya <rastorguew94@gmail.com>
2018-09-16 23:32:04 +0300

    add some auth classes

---
+++ app/src/main/java/com/diabetest/auth/AuthService.java
@@ -0,0 +1,20 @@
+package com.diabetest.auth;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class AuthService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
